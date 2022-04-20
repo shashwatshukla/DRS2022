ffc95caa96614fc9604852cdfcf1d7222d615093
Shashwat <shashwat.link@gmail.com>
2022-04-20 11:44:20 +0530

    All group fleet list is ow calculated as per the entries in fleet DB

--- test.py
+++ test.py
@@ -1,19 +1,11 @@
-# import goto
+import goto
 import streamlit as st, pandas as pd
 from load_Data import get_data
 import sqlite3
 df_drsend = get_data(r'database/mms_master.sqlite','drsend')
 df_vessel = get_data(r'database/mms_master.sqlite','vessels')
 df_fleet = get_data(r'database/mms_master.sqlite','fleet')
-
-flt_no = list(df_fleet['fltNameUID'])
-flt_name = list(df_fleet['fltLocalName'])
-for e_no, e_name in zip(flt_no,flt_name):
-    print(e_no, e_name)
-
-st.write(flt_no)
-st.write(flt_name)
-
+flt_list=dict(df_fleet[['fltLocalName','fltNameUID']].values)
 
 
 df_merged = pd.merge(df_drsend,df_vessel[['vsl_imo','statusActiveInactive','vslFleet']], on = 'vsl_imo',how = 'left') # brig col from vessel to drsend dataframe
@@ -27,9 +19,7 @@ st.write(df_merged)
 #st.write(missingships)
 # missingfleet = list(df_nan2['ship_name'].unique())
 #st.write(missingfleet)
-#m = df_merged['vslFleet']=='1'
-#tanker1=df_merged['ship_name'].mask(m).dropna().unique()
-fleetName = {'Tanker 1':1, 'Tanker 2: SG':2, }
+group_wise={list(flt_list.keys())[i]:sorted(list(df_merged.loc[df_merged['vslFleet'] == str(list(flt_list.values())[i])
+,'ship_name'].unique())) for i in range(len(flt_list))} # all vesssel fleet wise using dict comprehension
+st.write(group_wise)
 
-tanker1={"Tanker 1":[df_merged.loc[df_merged['vslFleet'] == '1','ship_name'].unique()]} # and so on.....
-st.write(tanker1)
