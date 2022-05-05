5450ac1cefffc6bed8595e0960e208fbf73babf4
Orca101 <shashwat.link@gmail.com>
2022-05-06 00:01:10 +0900

    Solved problem of Excel. Now closes only the instance opened by xlwings. Levs others intact. Added progress bar for preparation of DRS sheet

--- GetNewDRS.py
+++ GetNewDRS.py
@@ -5,13 +5,10 @@ from load_Data import get_data
 import xlwings as xw
 from datetime import date
 from io import BytesIO
-import os, time
+import os, time, pythoncom, win32com
 
 disp_cols = []
 output = BytesIO
-
-
-
 
 def make_NewDRS():
     # st.set_page_config(page_title='Generate new DR sender', layout='wide')
@@ -27,7 +24,7 @@ def make_NewDRS():
             return xlsmbyte
 
     curr_year = str(datetime.datetime.now().year)
-    print(curr_year)
+    print(curr_year,'--------------------------')
     st.markdown(f'Generate new **{curr_year} DR sender**')
     df_rawData = get_data(r'database/mms_master.sqlite', 'drsend')  # get raw data to work upon
     vsl_list = sorted(list(df_rawData['ship_name'].unique()))
@@ -44,21 +41,26 @@ def make_NewDRS():
         df_currDRS = df_rawData.query("ship_name == @shipName and (dt_ocurred.str.contains(@curr_year) or done_dt.str.contains(@curr_year) or status.str.contains('OPEN'))", engine='python')
 
         numberOfRows = len(df_currDRS)
+        jobDone = st.progress(0)
         filename = r'_DRS V56.xlsm'  #
-        book = xw.Book(filename, password='mms@user')  # Get template file
-        ws = book.sheets['DRSEND']
-        app = xw.apps.active
-        #app = xw.apps.active
-        for i in range(numberOfRows):
-            ws.range('A8:CZ8').insert(shift='down',
-                                      copy_origin='format_from_left_or_above')  # shift named ranges in excel to prevent overwriting
-        ws.range('C1').value = shipName
-        f_name = str(date.today()) + ' ' + shipName + ' DRS56.xlsm'
-        new_drs_file = os.path.join('temp', f_name)  # save in temp folder
-        ws.range('A8').options(index=False, header=False).value = df_currDRS  # write dataframe to excel
-        f = book.save(new_drs_file)  # save excel as new file
-        book.close()
-        app.kill()
+        xl = win32com.client.Dispatch("Excel.Application", pythoncom.CoInitialize())
+        with xw.App(visible=False) as app:
+            book = xw.Book(filename, password='mms@user')  # Get template file
+            ws = book.sheets['DRSEND']
+            # app = xw.apps.active
+            for i in range(numberOfRows):
+                ws.range('A8:CZ8').insert(shift='down',
+                                          copy_origin='format_from_left_or_above')  # shift named ranges in excel to prevent overwriting
+                jobDone.progress(int(i/numberOfRows*100))
+            ws.range('C1').value = shipName
+            f_name = str(date.today()) + ' ' + shipName + ' DRS56.xlsm'
+            new_drs_file = os.path.join('temp', f_name)  # save in temp folder
+            ws.range('A8').options(index=False, header=False).value = df_currDRS  # write dataframe to excel
+            f = book.save(new_drs_file)  # save excel as new file
+            book.close()
+            print (f'saved: {new_drs_file}')
+            jobDone.progress(100)
+        # app.quit()
 
         with col2:
             st.info(f'Done... {shipName} DR Sender has {len(df_currDRS)} entries for {curr_year}.')
