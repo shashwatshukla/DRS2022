5e5810cac580e402d154210e4853b4fe677ed47c
Shashwat <shashwat.link@gmail.com>
2022-05-09 14:39:47 +0530

    revert back to original filter data functionality. Download DR sender page now has form

--- GetNewDRS.py
+++ GetNewDRS.py
@@ -28,49 +28,54 @@ def make_NewDRS():
     st.markdown(f'Generate new **{curr_year} DR sender**')
     df_rawData = get_data(r'database/mms_master.sqlite', 'drsend')  # get raw data to work upon
     vsl_list = sorted(list(df_rawData['ship_name'].unique()))
+    with st.form(key='Process'):
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            shipName = st.selectbox('Select Vessel', vsl_list)# Choose vessel from list of shipnames
 
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        shipName = st.selectbox('Select Vessel', vsl_list)# Choose vessel from list of shipnames
-    bt=st.button('Proceed')
-    if bt:
-        st.markdown(f'{shipName} DR Sender being prepared. Please wait....')
-        df_rawData[['delay_hr', 'downtime_hr', 'VET_risk']] = df_rawData[['delay_hr', 'downtime_hr', 'VET_risk']] \
-            .apply(pd.to_numeric, errors='coerce', axis=1)  # Convert to prevent errors
+        btn = st.form_submit_button('Confirm')
+    #bt=st.button('Proceed')
+        if btn:
+            st.markdown(f'{shipName} DR Sender being prepared. Please wait....')
+            df_rawData[['delay_hr', 'downtime_hr', 'VET_risk']] = df_rawData[['delay_hr', 'downtime_hr', 'VET_risk']] \
+                .apply(pd.to_numeric, errors='coerce', axis=1)  # Convert to prevent errors
 
-        df_currDRS = df_rawData.query("ship_name == @shipName and (dt_ocurred.str.contains(@curr_year) or done_dt.str.contains(@curr_year) or status.str.contains('OPEN'))", engine='python')
+            df_currDRS = df_rawData.query("ship_name == @shipName and (dt_ocurred.str.contains(@curr_year) or done_dt.str.contains(@curr_year) or status.str.contains('OPEN'))", engine='python')
 
-        numberOfRows = len(df_currDRS)
-        jobProgress = st.progress(0)
-        filename = r'_DRS V56.xlsm'  #
-        xl = win32com.client.Dispatch("Excel.Application", pythoncom.CoInitialize())
-        with xw.App(visible=False) as app:
-            book = xw.Book(filename, password='mms@user')  # Get template file
-            ws = book.sheets['DRSEND']
-            # app = xw.apps.active
-            for i in range(numberOfRows):
-                ws.range('A8:CZ8').insert(shift='down',
-                                          copy_origin='format_from_left_or_above')  # shift named ranges in excel to prevent overwriting
-                jobProgress.progress(int(i/numberOfRows*100))
-            ws.range('C1').value = shipName
-            f_name = str(date.today()) + ' ' + shipName + ' DRS56.xlsm'
-            new_drs_file = os.path.join('temp', f_name)  # save in temp folder
-            ws.range('A8').options(index=False, header=False).value = df_currDRS  # write dataframe to excel
-            f = book.save(new_drs_file)  # save excel as new file
-            book.close()
-            print (f'saved: {new_drs_file}')
-            jobProgress.progress(100)
-        # app.quit()
+            numberOfRows = len(df_currDRS)
+            jobProgress = st.progress(0)
+            filename = r'_DRS V56.xlsm'  #
+            xl = win32com.client.Dispatch("Excel.Application", pythoncom.CoInitialize())
+            with xw.App(visible=False) as app:
+                book = xw.Book(filename, password='mms@user')  # Get template file
+                ws = book.sheets['DRSEND']
+                # app = xw.apps.active
+                for i in range(numberOfRows):
+                    ws.range('A8:CZ8').insert(shift='down',
+                                              copy_origin='format_from_left_or_above')  # shift named ranges in excel to prevent overwriting
+                    jobProgress.progress(int(i/numberOfRows*100))
+                ws.range('C1').value = shipName
 
-        with col2:
-            st.info(f'Done... {shipName} DR Sender has {len(df_currDRS)} entries for {curr_year}.')
+                f_name = str(date.today()) + ' ' + shipName + ' DRS56.xlsm'
+                new_drs_file = os.path.join('temp', f_name)  # save in temp folder
+                ws.range('A8').options(index=False, header=False).value = df_currDRS  # write dataframe to excel
+                f = book.save(new_drs_file)  # save excel as new file
+                book.close()
+                print (f'saved: {new_drs_file}')
+                jobProgress.progress(100)
+            # app.quit()
 
-    #app.quit()
+            with col2:
+                st.info(f'Done... {shipName} DR Sender has {len(df_currDRS)} entries for {curr_year}.')
+            st.write(df_currDRS[disp_cols])  # display only selected columns
+
+        #app.quit()
+    if btn:
         st.download_button(label=f"Download {shipName} DR sender",
-                           data=read_file(new_drs_file),
-                           file_name=new_drs_file[4:],
-                           mime='application/vns.ms-excel')
-        st.write(df_currDRS[disp_cols])  # display only selected columns
+                               data=read_file(new_drs_file),
+                               file_name=new_drs_file[4:],
+                               mime='application/vns.ms-excel')
+
 
 
 
