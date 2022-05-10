5c3184fa8b07638b750b22788d52e039631a7c3f
Shashwat <shashwat.link@gmail.com>
2022-05-10 14:33:51 +0530

    modified main.py

--- main.py
+++ main.py
@@ -6,16 +6,29 @@ from filter_Data import filtered_Data
 from UploadDRS import upload_drs
 from Dashboard_drs import dashboard
 conn=sq.connect(r'database/mms_master.sqlite')
-df_mailid=pd.read_sql_query('select siEmail from si', conn)
+df_mailid=pd.read_sql_query('select siName,siEmail from si', conn)
 conn.close()
 col1, col2,col3=st.columns(3)
-with col3:
+allmailid = df_mailid['siEmail'].tolist()
 
-    mailid = st.text_input("MMS email id",value='@mmstokyo.co.jp')
+mailid = st.sidebar.text_input("MMS email id",value='@mmstokyo.co.jp')
 #if 'id' not in st.session_state:
 st.session_state.id = mailid
-allmailid = df_mailid['siEmail'].tolist()
-if(mailid in allmailid):
+# allmailid = df_mailid['siEmail'].tolist()
+# mailid = st.text_input("MMS email id", value='@mmstokyo.co.jp')
+person = df_mailid[df_mailid['siEmail'] == mailid]
+
+# if "emailid" not in st.session_state:
+#     with col3:
+#         if (len(person)==1):
+#             st.session_state.emailid = mailid
+# else:
+#     mailid = st.session_state.emailid
+#     person = df_mailid[df_mailid['siEmail']== mailid]
+
+# st.write(person)
+if len(person==1):
+    st.sidebar.info(f'Welcome {person.iloc[0, 0]}')
     sb_sel = st.sidebar.radio('Select Page', options=['View/Filter Data', 'Download DR sender', 'Upload DR sender',
                                                       'Dashboard(In progress)'])
     if sb_sel == 'Download DR sender':
@@ -27,5 +40,4 @@ if(mailid in allmailid):
     if sb_sel == 'Dashboard(In progress)':
         dashboard()
 else:
-    with col3:
-        st.warning('MMS mail id required to proceed......')
+    st.sidebar.info('MMS mail id required to proceed......')
