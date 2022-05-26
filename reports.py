b9554e0318133a978c428735c47eb431b0ee21c5
Shashwat <shashwat.link@gmail.com>
2022-05-26 06:52:59 +0530

    Query building continued.

--- reports.py
+++ reports.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 from helpers import get_data, get_vessel_byfleet
 import datetime
+from datetime import timedelta
 st.set_page_config(page_title='DR Sender', layout='wide')
 # ___________________________Declarations_____________________________
 curr_year = str(datetime.datetime.now().year)
@@ -28,8 +29,15 @@ vslListPerFlt = sum([vsl_list_fleetwise[x] for x in fltName], []) # get vsl name
 vslName = st.multiselect('Select the vessel:', options=sorted(vslListPerFlt), default=sorted(vslListPerFlt))
 df_active_ships_currDRS = df_active_ships.query("ship_name == @vslName and (dt_ocurred.str.contains(@curr_year)"
                               " or done_dt.str.contains(@curr_year) or status.str.contains('OPEN'))", engine='python')
-
-df_active_ships_currDRS[['dt_ocurred','target_dt', 'done_dt','ext_dt']]=df_active_ships[['dt_ocurred','target_dt', 'done_dt','ext_dt']].apply(pd.to_datetime)
-df_active_ships_currDRS['dt_today']=todaydt
-df_active_ships_currDRS=df_active_ships_currDRS.query("ship_name == @vslName and target_dt<dt_today and status=='OPEN'", engine='python')
-st.write(df_active_ships_currDRS)
+df_active_ships_currDRS['dt_today']=str(todaydt)
+def convert(dt):
+    return datetime.datetime.strptime(dt, "%Y-%m-%d")
+df_active_ships_currDRS['dt_ocurred']=df_active_ships_currDRS['dt_ocurred'].apply(convert)
+df_active_ships_currDRS['dt_today']=df_active_ships_currDRS['dt_today'].apply(convert)
+#df_active_ships_currDRS['done_dt']=df_active_ships_currDRS['done_dt'].apply(convert)
+#df_active_ships_currDRS['ext_dt']=df_active_ships_currDRS['ext_dt'].apply(convert)
+df_open_past_target=df_active_ships_currDRS.query("ship_name == @vslName and target_dt<dt_today and status=='OPEN'", engine='python')
+df_open_past_90=df_active_ships_currDRS[(df_active_ships_currDRS.ship_name.isin(vslName)) & (df_active_ships_currDRS.dt_ocurred+timedelta(days=90)<df_active_ships_currDRS.dt_today) & (df_active_ships_currDRS.status=='OPEN')]
+df_closed_od=df_active_ships_currDRS[(df_active_ships_currDRS.ship_name.isin(vslName)) & (df_active_ships_currDRS.dt_ocurred+timedelta(days=90)<df_active_ships_currDRS.done_dt) & (df_active_ships_currDRS.status=='CLOSE')]
+df_open_past_90.groupby("ship_name")["status"].count()
+st.write(df_open_past_90.groupby("ship_name")["status"].count())
